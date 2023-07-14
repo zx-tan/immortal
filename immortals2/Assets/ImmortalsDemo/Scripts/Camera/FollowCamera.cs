0b0a8a18c85d3ddff420be7b9fc0a58001727e0c
jx.tan <gml.jx.tan@proton.me>
2023-07-14 15:43:00 +0800

    feat(FollowCamera)
    implemented camera to follow a target gameobject

--- immortals2/Assets/ImmortalsDemo/Scripts/Camera/FollowCamera.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/Camera/FollowCamera.cs
@@ -6,11 +6,29 @@ public class FollowCamera : MonoBehaviour, IFollow
     private Transform target;
     public Transform Target { get { return target; } }
 
+    public float xRotation;
+    public float yRotation;
+    public bool useTargetYRotation;
+    public Vector3 targetOffset;
+    public float zoomDistance = 10.0f;
+
     private void Update()
     {
         // Early out if we don't have a target
-        if (!target) return;
+        if (!Target) return;
 
         // implement follow
+        var wantedPosition = Target.position + targetOffset;
+        var wantedYRotation = useTargetYRotation ? Target.eulerAngles.y : yRotation;
+
+        // Convert the angle into a rotation
+        var currentRotation = Quaternion.Euler(xRotation, wantedYRotation, 0);
+
+        // Set the position of the camera on the x-z plane to:
+        // distance meters behind the target
+        wantedPosition -= currentRotation * Vector3.forward * zoomDistance;
+
+        // Update position
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, damping * Time.deltaTime);
     }
 }
