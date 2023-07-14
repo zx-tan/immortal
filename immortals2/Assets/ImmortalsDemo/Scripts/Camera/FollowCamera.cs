6dc51151920e0b1cf2d38235859213235ff71751
jx.tan <gml.jx.tan@proton.me>
2023-07-14 12:01:00 +0800

    feat(FollowCamera)
    added implementation

--- immortals2/Assets/ImmortalsDemo/Scripts/Camera/FollowCamera.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/Camera/FollowCamera.cs
@@ -5,4 +5,12 @@ public class FollowCamera : MonoBehaviour, IFollow
     [SerializeField]
     private Transform target;
     public Transform Target { get { return target; } }
+
+    private void Update()
+    {
+        // Early out if we don't have a target
+        if (!target) return;
+
+        // implement follow
+    }
 }
