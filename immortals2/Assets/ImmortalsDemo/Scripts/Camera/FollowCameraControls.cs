90d78821a63e3e1ce3241c6b9072876c8be7cc71
qy.woon <gml.qy.woon@proton.me>
2023-07-19 16:14:53 +0800

    feat(FollowCameraControls)
    added following implementation

--- immortals2/Assets/ImmortalsDemo/Scripts/Camera/FollowCameraControls.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/Camera/FollowCameraControls.cs
@@ -2,5 +2,22 @@
 
 public class FollowCameraControls : MonoBehaviour
 {
-    
+    private Camera targetCamera;
+    private FollowCamera targetFollowCamera;
+
+    void Awake()
+    {
+        if (targetCamera == null)
+            targetCamera = Camera.main;
+
+        targetFollowCamera = targetCamera.gameObject.GetComponent<FollowCamera>();
+        if (targetFollowCamera == null)
+            targetFollowCamera = targetCamera.gameObject.AddComponent<FollowCamera>();
+    }
+
+    void LateUpdate()
+    {
+        targetFollowCamera.target = target;
+        targetFollowCamera.targetOffset = targetOffset;
+    }
 }
