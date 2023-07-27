1dec5c5a98721220039799e3ed4cfb46248503ce
gml.dj.png <gml.dj.png@proton.me>
2023-07-27 17:04:52 +0800

    feat(CharacterModel)
    added character model class to setup character

---
+++ immortals2/Assets/ImmortalsDemo/Scripts/GamePlay/CharacterModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterModel : MonoBehaviour
+{
+    [SerializeField] private Transform headContainer;
+
+    private Animator tempAnimator
+    private GameObject headModel;
+    private readonly List<GameObject> weaponModels = new List<GameObject>();
+
+    private void Start()
+    {
+        tempAnimator = GetComponent<Animator>();
+    }
+
+    public void SetHeadModel(GameObject model)
+    {
+        if (headModel != null)
+            Destroy(headModel);
+        headModel = AddModel(model, headContainer, null);
+    }
+}
