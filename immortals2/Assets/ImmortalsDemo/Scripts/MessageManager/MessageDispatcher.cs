d200d31e76d45fba87e229b9288255276cd6f93a
ck.liang <gml.ck.liang@proton.me>
2023-08-07 13:58:34 +0800

    feat(MessageDispatcher)
    added dispatcher

---
+++ immortals2/Assets/ImmortalsDemo/Scripts/MessageManager/MessageDispatcher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Immortal
+{
+	public class MessageDispatcher<M> where M : Message
+	{
+		public interface IListener
+		{
+			void OnMessage(M msg);
+		}
+	}
+}
