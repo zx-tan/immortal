2efe491e5d03b6cbf7a725851a346d82f9f90a07
ck.liang <gml.ck.liang@proton.me>
2023-08-10 15:19:44 +0800

    feat(MessageDispatcher)
    implemented registration

--- immortals2/Assets/ImmortalsDemo/Scripts/MessageManager/MessageDispatcher.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/MessageManager/MessageDispatcher.cs
@@ -11,5 +11,46 @@ namespace Immortal
 		{
 			void OnMessage(M msg);
 		}
+
+		private Dictionary<Type, List<IListener>> listeners = new Dictionary<Type, List<IListener>>();
+
+		public void RegisterListener<T>(IListener listener) where T : M
+		{
+			RegisterListener(typeof(T), listener);
+		}
+
+		public void UnregisterListener<T>(IListener listener)
+		{
+			UnregisterListener(typeof(T), listener);
+		}
+
+		private void RegisterListener(Type msgType, IListener listener)
+		{
+			if (!listeners.ContainsKey(msgType))
+			{
+				listeners.Add(msgType, new List<IListener>());
+			}
+
+			if (!listeners[msgType].Contains(listener))
+			{
+				listeners[msgType].Add(listener);
+			}
+		}
+
+		private void UnregisterListener(Type msgType, IListener listener)
+		{
+			if (listeners.TryGetValue(msgType, out var node))
+			{
+				if (node.Contains(listener))
+				{
+					node.Remove(listener);
+				}
+				
+				if (node.Count == 0)
+				{
+					listeners.Remove(msgType);
+				}
+			}
+		}
 	}
 }
