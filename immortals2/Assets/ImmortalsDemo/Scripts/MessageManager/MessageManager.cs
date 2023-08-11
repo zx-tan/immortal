a61f06c635ba2f1414b7413e764be05f953bf0d3
qy.woon <gml.qy.woon@proton.me>
2023-08-11 12:58:13 +0800

    feat(MessageManager)
    implemented registration and dispatch

--- immortals2/Assets/ImmortalsDemo/Scripts/MessageManager/MessageManager.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/MessageManager/MessageManager.cs
@@ -7,20 +7,21 @@ namespace Immortal
 	{
 		public interface IListener : MessageDispatcher<Message>.IListener { }
 
-		private MessageDispatcher<Message> _messages = new MessageDispatcher<Message>();
+		private MessageDispatcher<Message> _messageDispatchers = new MessageDispatcher<Message>();
 
 		public void RegisterListener(IListener listener)
 		{
-			
+			_messageDispatchers.RegisterListener(typeof(T), listener);
 		}
 
 		public void UnregisterListener(IListener listener)
 		{
-			
+			_messageDispatchers.UnregisterListener(typeof(T), listener);
 		}
 
-		public void Dispatch(GameMessage msg)
+		public void Dispatch(Message msg)
 		{
+			_messageDispatchers.Dispatch(msg);
 		}
 		
 		public void DelayedDispatch(float delay, Message msg)
