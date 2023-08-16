22251148142a1fa019a007eab925163e53fde3b8
dj.png <gml.dj.png@proton.me>
2023-08-16 13:41:47 +0800

    feat(UI)
    implemented display method

--- immortals2/Assets/ImmortalsDemo/Scripts/UI/UIViewControllerBase.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/UI/UIViewControllerBase.cs
@@ -6,6 +6,9 @@ namespace Immortals
 
 	public abstract class UIViewControllerBase : Monobehaviour
 	{
-		
+		public void DisplayView(bool display)
+		{
+			this.gameObject.SetActive(display)
+		}
 	}
 }
