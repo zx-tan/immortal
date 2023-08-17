c3014fa503054d6624ac47c2288e5db133f29fd6
dj.png <gml.dj.png@proton.me>
2023-08-17 10:27:08 +0800

    feat(UI)
    fixed compile error from inheriting interface

--- immortals2/Assets/ImmortalsDemo/Scripts/UI/UIViewManager.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/UI/UIViewManager.cs
@@ -6,6 +6,14 @@ namespace Immortals
 
 	public abstract class UIViewManager : Monobehaviour, IUIViewManager
 	{
-		
+		public void AddView(UIViewControllerBase ui)
+		{
+
+		}
+
+		public void RemoveView(UIViewControllerBase ui)
+		{
+
+		}
 	}
 }
