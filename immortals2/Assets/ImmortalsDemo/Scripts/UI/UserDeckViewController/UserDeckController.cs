772fac7b9869f9fb16972936eef704885d8c0c9b
jx.tan <gml.jx.tan@proton.me>
2023-08-22 09:55:47 +0800

    feat(UI)
    added deck view

---
+++ immortals2/Assets/ImmortalsDemo/Scripts/UI/UserDeckViewController/UserDeckController.cs
@@ -0,0 +1,12 @@
+﻿using NullPointerCore;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Immortals
+{
+	public class UserDeckController : UIViewControllerBase
+	{
+		
+	}
+}
