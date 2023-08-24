f13bb3af1328fb276e297534a4ab76b07dab2bb7
jx.tan <gml.jx.tan@proton.me>
2023-08-24 16:02:08 +0800

    feat(UI)
    implementing

--- immortals2/Assets/ImmortalsDemo/Scripts/UI/UserDeckViewController/UserDeckController.cs
+++ immortals2/Assets/ImmortalsDemo/Scripts/UI/UserDeckViewController/UserDeckController.cs
@@ -7,6 +7,52 @@ namespace Immortals
 {
 	public class UserDeckController : UIViewControllerBase
 	{
+		private enum DECK_VIEW {
+			ATTACK_VIEW,
+			DEFENSE_VIEW
+		}
+
+		private DECK_VIEW _deckView;
+		private GameController gameController;
+		private PlayerDeck playerDeck;
+		private Player LocalPlayer { get { return Game.GetSystem<GameController>().localPlayer; } }
+
+		protected PlayerDeck Deck 
+		{
+			get
+			{
+				if (playerDeck == null && LocalPlayer != null)
+					playerDeck = LocalPlayer.GetComponent<PlayerDeck>();
+				return playerDeck;
+			}
+		}
+
+		public void OnEnable()
+		{
+			if (LocalPlayer != null)
+			{
+				playerDeck = LocalPlayer.GetComponent<PlayerDeck>();
+			}
+
+			gameController = Game.GetSystem<GameController>();
+		}
+
+		public void OnViewDefenseDeck()
+		{
+			if (gameController != null)
+			{
+				_deckView = DEFENSE_VIEW;
+			}
+		}
+
+		public void OnViewAttackDeck()
+		{
+			if (gameController != null)
+			{
+				_deckView = ATTACK_VIEW;
+			}
+		}
+
 		public void ResetView()
 		{
 
